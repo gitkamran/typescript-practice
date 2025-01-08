3e6eab583685458339a04c22e0480478f819bc10
gitkamran <mkamran.ir@gmail.com>
2025-01-08 18:26:21 +0330

    add type and set to session

--- components/practice/MainPractice.tsx
+++ components/practice/MainPractice.tsx
@@ -1,11 +1,33 @@
+"use client";
 import React from "react";
 
+type InformationMe = {
+  firstname: string;
+  lastname: string;
+  age: number;
+  job: string;
+};
+
 const MainPractice = () => {
-  const firstname: string = "محمد";
-  const lastname: string = "کامران";
+  const informationMe: InformationMe = {
+    firstname: "محمد",
+    lastname: "کامران",
+    age: 32,
+    job: "برنامه نویس",
+  };
+
+  sessionStorage.setItem("data", JSON.stringify(informationMe));
 
   return (
-    <div className="flex items-start h-full">{firstname + " " + lastname}</div>
+    <div className="flex items-start h-full">
+      <div className="flex flex-col gap-4 p-2 text-sm">
+        <span>
+          {informationMe.firstname} {informationMe.lastname}
+        </span>
+        <span>سن: {informationMe.age}</span>
+        <span>شغل: {informationMe.job}</span>
+      </div>
+    </div>
   );
 };
 
